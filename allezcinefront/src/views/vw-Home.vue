ddda8ffd6efb0ba7e820195f0257a0b78abf4928
Maxime Broodcoorens <broodcoorens.maxime@gmail.com>
2019-04-02 15:14:08 +0200

    chore - added routes & imported axios

---
+++ allezcinefront/src/views/vw-Home.vue
@@ -0,0 +1,14 @@
+<template>
+    
+</template>
+
+<script>
+export default {
+    name: vw-Home
+
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ allezcinefront/src/views/vw-Series.vue
@@ -0,0 +1,14 @@
+<template>
+    
+</template>
+
+<script>
+export default {
+    name: vw-Series
+
+}
+</script>
+
+<style scoped>
+
+</style>
