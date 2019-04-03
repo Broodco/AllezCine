3b21d10688cd9ff5c8abe138dcdd4df00cd58225
massart <f_massart@hotmail.com>
2019-04-03 09:49:44 +0200

    resolution stash carousel

--- allezcinefront/src/components/carousel/cp-Carousel.vue
+++ allezcinefront/src/components/carousel/cp-Carousel.vue
@@ -1,9 +1,29 @@
 <template>
-    
+    <!-- <v-carousel hide-delimiters>
+      <v-carousel-item
+        v-for="(item,i) in items"
+        :key="i"
+        :src="item.src"
+      ></v-carousel-item>
+   </v-carousel> -->
+   <div id="app">
+        {{ info }}
+   </div>
 </template>
 
 <script>
 export default {
+    data () {
+    return {
+      info: null
+    }
+  },
+  mounted () {
+    axios
+      .get('https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=833ff06d69182d00cff97e3090365785')
+      .then(response => (this.info = response))
+  }
+
 
 }
 </script>
