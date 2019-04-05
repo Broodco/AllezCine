2773fbc863bebb8a09a4ca275967ffbdf2f5b88e
massart <f_massart@hotmail.com>
2019-04-05 13:59:32 +0200

    footer flex

--- allezcinefront/src/components/footer/cp-Footer.vue
+++ allezcinefront/src/components/footer/cp-Footer.vue
@@ -1,31 +1,33 @@
 <template>
-    <div>
-        <v-footer
-        dark
-        height="auto"
-        align-center
-        >
-            <v-container>
-                <v-layout justify-space-around align-center row xs-12 md-4>
-                    <v-flex 
-                    align-self-center
-                    lg4 sm12
-                    align-content-center>
-                    Hello
-                    </v-flex>
-                    <v-flex 
-                    align-self-center
-                    lg4 sm12>
-                    Hello
-                    </v-flex>
-                    <v-flex 
-                    align-self-center
-                    lg4 sm12>
-                    Hello
-                    </v-flex>
-                </v-layout>
-            </v-container>
-        </v-footer>
+    <div class="footer">
+        <div class="titre">
+            <h1>ALLEZ<span>CINE!</span></h1>
+        </div>
+        <div class="soustitre">
+            <div class="fondamentaux">
+                <h2>LES FONDAMENTAUX</h2>
+            </div>
+            <div class="communauté">
+                <h2>COMMUNAUTE</h2>
+            </div>
+            <div class="légal">
+                <h2>LEGAL</h2>
+            </div>
+        </div>
+        <div class="groupe">
+            <div class="groupefond">
+                <p>A propros de Allezciné</p>
+                <p>contactez-nous</p>
+                <p>Forum d'aide</p>
+
+            </div>
+            <div class="groupecomm">
+
+            </div>
+            <div class="groupelegal">
+
+            </div>
+        </div>
     </div>
 </template>
 
